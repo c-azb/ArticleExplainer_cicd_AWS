12c35bef7ab3a095980f4bf66f77002dc1502f70
c-azb <carlos1997.cazb@gmail.com>
2025-10-12 10:07:16 -0300

    first commit

---
+++ Backend_app/main.py
@@ -0,0 +1,31 @@
+
+from dotenv import load_dotenv
+import os
+load_dotenv()#if not load_dotenv(): print("Failed loading dotenv") 
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.API.Routers import graph_router
+from src.API.Routers import user_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.getenv('ALLOW_ORIGINS').split(), #use "*" for any or the website url hosted in prduction... "http://localhost:5173","http://127.0.0.1:5173"
+    allow_credentials=True,
+    allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # Allow all headers
+)
+
+app.include_router(graph_router.graph_router)
+app.include_router(user_router.user_router)
+
+#uvicorn main:app --reload
+# if __name__=="__main__":
+#     import uvicorn
+#     uvicorn.run(app,host="127.0.0.1",port=8000)
+
+#needed for AWS lambda deployment #pip install -t ./package_directory -r requirements.txt
+from mangum import Mangum
+handler = Mangum(app)
---
+++ Frontend_app/frontend_app/src/constants/constants.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "http://127.0.0.1:8000";
+
+export const LOGIN_API_ENDPOINT = API_BASE_URL + "/user/login";
+export const LOGOUT_API_ENDPOINT = API_BASE_URL + "/user/logout";
+export const REFRESH_API_ENDPOINT = API_BASE_URL + "/user/refresh";
+export const REGISTER_API_ENDPOINT = API_BASE_URL + "/user/register";
+
+export const REQUEST_GRAPH_ENDPOINT = API_BASE_URL + "/graph";
+export const GET_EXPLANATIONS_ENDPOINT = REQUEST_GRAPH_ENDPOINT + "/explanations";
+export const GET_EXPLANATION_ENDPOINT = REQUEST_GRAPH_ENDPOINT + "/explanation";
